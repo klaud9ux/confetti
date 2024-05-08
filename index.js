5711861ef355ec2d5d63f98eb9a4b6963652622d
klaud9 <klaud9@userinsight.co.kr>
2024-05-08 17:27:54 +0900

    school parade

---
+++ index.js
@@ -0,0 +1,98 @@
+import Particle from "./js/Particle.js"
+
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+const dpr = window.devicePixelRatio > 1 ? 2 : 1
+let canvasWidth = innerWidth
+let canvasHeight = innerHeight
+const interval = 1000 / 60
+
+const particles = []
+
+function init() {
+  canvasWidth = innerWidth
+  canvasHeight = innerHeight
+  canvas.style.width = canvasWidth + 'px'
+  canvas.style.height = canvasHeight + 'px'
+  canvas.width = canvasWidth * dpr
+  canvas.height = canvasHeight * dpr
+  ctx.scale(dpr, dpr)
+
+  confetti({
+    x: canvasWidth / 2,
+    y: canvasHeight / 2,
+    count: 10
+  })
+}
+
+function confetti({ x, y, count, deg, colors, shapes, spread }) {
+  for (let i = 0; i < count; i++){
+    particles.push(new Particle(x, y, deg, colors, shapes, spread))
+  }
+}
+function render() {
+  let now, delta
+  let then = Date.now()
+
+  let deg = 0
+  
+  const frame = () => {
+    requestAnimationFrame(frame)
+    now = Date.now()
+    delta = now - then
+    if(delta < interval) return
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+
+    deg += 1
+
+    confetti({
+      x: 0.5, // 0 ~ 1
+      y: 0.5, // 0 ~ 1
+      count: 5,
+      deg: 225 + deg,
+      spread: 1
+    })
+    confetti({
+      x: 0.5, // 0 ~ 1
+      y: 0.5, // 0 ~ 1
+      count: 5,
+      deg: 90 + deg,
+      spread: 1
+    })
+    confetti({
+      x: 0.5, // 0 ~ 1
+      y: 0.5, // 0 ~ 1
+      count: 5,
+      deg: 315 + deg,
+      spread: 1
+    })
+    
+
+    for (let i = particles.length - 1; i >= 0; i--) {
+      particles[i].update()
+      particles[i].draw(ctx)
+
+      if (particles[i].opacity < 0) particles.splice(i, 1)
+      if (particles[i].y > canvasHeight) particles.splice(i, 1)
+    }
+
+    then = now - (delta % interval)
+  }
+  requestAnimationFrame(frame)
+  
+}
+window.addEventListener('click', () => {
+  confetti({
+    x: 0, // 0 ~ 1
+    y: 0.5, // 0 ~ 1
+    count: 10,
+    deg: -50,
+    spread: 1
+    // colors: ['#FF0000']
+  })
+})
+window.addEventListener('resize', init)
+window.addEventListener('load', () => {
+  init()
+  render()
+})
